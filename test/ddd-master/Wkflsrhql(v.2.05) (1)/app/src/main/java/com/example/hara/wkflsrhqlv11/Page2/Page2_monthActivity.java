e2431941a04c5be7cf82c857c89756613c2a7c73
jang75967 <42910156+jang75967@users.noreply.github.com>
2018-10-03 19:55:36 +0900

    page2 -> 색상변화

--- test/ddd-master/Wkflsrhql(v.2.05) (1)/app/src/main/java/com/example/hara/wkflsrhqlv11/Page2/Page2_monthActivity.java
+++ test/ddd-master/Wkflsrhql(v.2.05) (1)/app/src/main/java/com/example/hara/wkflsrhqlv11/Page2/Page2_monthActivity.java
@@ -1,5 +1,6 @@
 package com.example.hara.wkflsrhqlv11.Page2;
 
+
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -31,7 +32,7 @@ public class Page2_monthActivity extends Fragment {
     public View view;
     public CreateID_UUID createID_uuid;
     String PhoneNum;
-    ArrayList<String> myDataset;
+    ArrayList<String> myDatasets;
     ArrayList<String> myGoalDataset;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,8 +44,9 @@ public class Page2_monthActivity extends Fragment {
         createID_uuid=new CreateID_UUID();
         PhoneNum=(String) createID_uuid.getUniqueID(view.getContext());
         Task task=new Task();
-        task.execute("List_day",PhoneNum);
-        task.execute("Get_list_goal",PhoneNum);
+        Task task2 = new Task();
+        task.execute("Goal_get_daysum",PhoneNum);
+        task2.execute("Get_list_goal",PhoneNum);
         //리스트
          return view;
 
@@ -52,79 +54,79 @@ public class Page2_monthActivity extends Fragment {
     }
 
     class Task extends AsyncTask<String, Void, String> {
-        Goal goal;
-
         protected void onPostExecute(String result) {
             if (result.charAt(0) == '1') {
-                //리스트 day 항목
-                // ArrayList<String> list_week = new ArrayList<String>();
-                int length = 4;
-                int count = 1;
-//                goal = new Goal();
-//                String[] result_split = result.split("<1>");
-//                String[] result_list;
-//
-//                while (count < result_split.length - 1) {
-//                    result_list = result_split[count].split("<br/>");
-//                    Log.i("GET_WEEK_LIST", "GET_WEEK_LIST : " + result_list[0]);
-//                    list_week.add(result_list[0]);
-//                count++;
-                    /*
-                    리스트 설정 파라미터는  result_list[0]
-                    */
-
-                goal = new Goal();
                 String[] result_split = result.split("<br/>");
                 String[] result_list;
-
+                myDatasets = new ArrayList<String>();
                 for(int i=1; i<result_split.length; i++){
                     result_list = result_split[i].split("<1>");
-                    myDataset.add(result_list[1]);
+                    myDatasets.add(result_list[1]);
                 }
-
-                // myDataset index(1-31)
-                //Goal.setList_WeekSum(list_week);
-                //int sum=0;
-                //for(int go=0; go<list_week.size(); go++)
-                  //  sum+= Integer.parseInt(list_week.get(go));
-
-                //TextView textView= (TextView)view.findViewById(R.id.month_goal);
-               //textView.setText(sum+"원 입니다");
-
             }
             else if (result.charAt(0) == '2') {
-                goal = new Goal();
-                String[] result_split = result.split("<br/>");
-                String[] result_list;
-
-                for(int i=1; i<result_split.length; i++){
-                    result_list = result_split[i].split("<1>");
-                    myGoalDataset.add(result_list[1]);
+                Log.i("Get_list_goal", "Get_list_goal_page2 : " +result);
+                String[] result_split2 = result.split("<br/>");
+                String[] result_list2;
+                myGoalDataset = new ArrayList<String>();
+                for(int i=1; i<result_split2.length; i++){
+                    result_list2 = result_split2[i].split("<1>");
+                    myGoalDataset.add(result_list2[1]);
                 }
-                String pre_array = "R.id.page2_day_";
-
-                for(int number =1; number < myDataset.size(); number++){
-                    pre_array+=""+(number);
-                    TextView textView= (TextView)view.findViewById(Integer.parseInt(pre_array));
-                    if(Integer.parseInt(myDataset.get(number)) < Integer.parseInt(myGoalDataset.get(number))){
-                        textView.setBackgroundColor(Color.argb(0,1,2,3));
+                int resID[] = {R.id.page2_day_1,R.id.page2_day_2,R.id.page2_day_3,R.id.page2_day_4,R.id.page2_day_5, R.id.page2_day_6, R.id.page2_day_7,
+                        R.id.page2_day_8, R.id.page2_day_9,R.id.page2_day_10,R.id.page2_day_11,R.id.page2_day_12,R.id.page2_day_13,R.id.page2_day_14,R.id.page2_day_15,
+                        R.id.page2_day_16,R.id.page2_day_17,R.id.page2_day_18,R.id.page2_day_19,R.id.page2_day_20,R.id.page2_day_21, R.id.page2_day_22, R.id.page2_day_23,
+                        R.id.page2_day_24,R.id.page2_day_25,R.id.page2_day_26,R.id.page2_day_27,R.id.page2_day_28,R.id.page2_day_29,R.id.page2_day_30,R.id.page2_day_31};
+                int count=0;
+                // 색상 목표치 이하일 때 초록색, 그 이상일 때 빨간색
+                for(int number =0; number < myDatasets.size(); number++){
+                    TextView textView= (TextView)view.findViewById(resID[number]);
+                    if(Integer.parseInt(myDatasets.get(number)) > Integer.parseInt(myGoalDataset.get(number))){
+                        Log.i("page2_myDatasets", "Integer.parseInt(myDatasets.get(number)) : " +Integer.parseInt(myDatasets.get(number)));
+                        Log.i("page2_myGoalDataset", "Integer.parseInt(myGoalDataset.get(number)) : " +Integer.parseInt(myGoalDataset.get(number)));
+                        textView.setBackgroundColor(Color.RED);
+                        count++;
                     }
                     else{
-                        textView.setBackgroundColor(Color.argb(0,1,2,3));
+                        Log.i("page2_myDatasets_else", "Integer.parseInt(myDatasets.get(number)) : " +Integer.parseInt(myDatasets.get(number)));
+                        Log.i("page2_myGoalDatast_else", "Integer.parseInt(myGoalDataset.get(number)) : " +Integer.parseInt(myGoalDataset.get(number)));
+//                        Log.i("page2_myGoalDatast_else", "Integer.parseInt(myGoalDataset.get(number)) : " +number);
+                        textView.setBackgroundColor(Color.GREEN);
                     }
                 }
-
+                //  1등급 ~ 6등급
+                // count <=5 1등급
+                // count <=10 2등급
+                // count <= 15 3등급
+                // count <= 20 4등급
+                // count <= 25 5등급
+                // count <= 31 6등급
+                Log.i("count_page2", "count " +count);
+                TextView textView2 = (TextView)view.findViewById(R.id.age);
+                if(count<=5)
+                    textView2.setText("1등급");
+                else if(count>5 && count <=10)
+                        textView2.setText("2등급");
+                else if(count>10 && count <=15)
+                        textView2.setText("3등급");
+                else if(count>15 && count <=20)
+                        textView2.setText("4등급");
+                else if(count>20 && count <=25)
+                        textView2.setText("5등급");
+                else if(count>25 && count <=31)
+                        textView2.setText("6등급");
+                else
+                        textView2.setText("6등급");
             }
         }
-
             @Override
             protected String doInBackground (String...params){
                 String method = params[0];
                 String errorString = null;
                 String User_Number=params[1];
-                if (method.equals("List_day")) {
+                if (method.equals("Goal_get_daysum")) {
                     try {
-                        URL url = new URL(Value.List_day);
+                        URL url = new URL(Value.Goal_get_daysum);
                         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                         httpURLConnection.setRequestMethod("POST");
                         httpURLConnection.setDoOutput(true);
